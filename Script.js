7afffe1a89fa6382c084549f9d05e865b6b8db42
sumitpant <38173704+sumitpant@users.noreply.github.com>
2020-10-03 21:13:48 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,10 +10,12 @@ localStorage.setItem("school",0);
 
 function add(src)
 {
-    alert("You added item")
+    //alert("You added item")
     
     //console.log(parseInt(localStorage.getItem(src))+1);
+    //localStorage.setItem(src,parseInt(localStorage.getItem(src))+1);
     localStorage.setItem(src,parseInt(localStorage.getItem(src))+1);
+    alert("You added " +localStorage.getItem(src)+ src+ " item ")
     //console.log(localStorage.getItem(src));
     
     // if(src==='clothing'){
